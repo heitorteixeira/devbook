da035f82dd9b922643ca3308b61d4dd3188ff8be
heitor.teixeira@compasso.com.br <heitor.teixeira@compasso.com.br>
2021-04-22 22:28:50 -0300

    Add validations, save new user and find user

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -4,40 +4,64 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
 )
 
 //CreateUser insert one user in database
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 	var user models.User
 	if err = json.Unmarshal(requestBody, &user); err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusBadRequest, err)
+		return
 	}
+	if err = user.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal("Connect error:", err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
+	defer db.Close()
 
 	repository := repositories.NewUserRepository(db)
-	userId, err := repository.Create(user)
+	user.ID, err = repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
-
-	w.Write([]byte(fmt.Sprintf("Id inserido: %d", userId)))
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 //FindAllUsers find all users in database
 func FindAllUsers(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Finding all users!"))
+	nameOrNick := strings.ToLower(r.URL.Query().Get("user"))
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	users, err := repository.Find(nameOrNick)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, users)
 }
 
 //FindUser find one user in database
--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //User represent one user using social media
 type User struct {
@@ -10,4 +14,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CriatedAt time.Time `json:"criatedAt,omitempty"`
+}
+
+//Prepare will call methods to validate and format User
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("Name is mandatory and cannot be empty")
+	}
+
+	if user.Nick == "" {
+		return errors.New("Nick is mandatory and cannot be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email is mandatory and cannot be empty")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password is mandatory and cannot be apty")
+	}
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
